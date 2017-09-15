bf48ef6c9dd75f045cc4c1b305b05ea57c8f817c
Obi Uchenna David <daviduchenna@outlook.com>
2017-09-15 01:32:20 +0100

    whitenoise for staticfiles

--- pyconngavi/urls.py
+++ pyconngavi/urls.py
@@ -18,10 +18,11 @@ from django.contrib import admin
 from app import views
 from django.conf import settings
 from django.conf.urls.static import static
-
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 urlpatterns = [
     url(r'^$', views.PyAvi.as_view()),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
+urlpatterns += staticfiles_urlpatterns()
--- pyconngavi/wsgi.py
+++ pyconngavi/wsgi.py
@@ -13,4 +13,7 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pyconngavi.settings")
 
+from whitenoise.django import DjangoWhiteNoise
+
 application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
