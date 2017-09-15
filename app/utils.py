988ff040cf1de6f27f0e9dde3a67669a1d60abf7
Obi Uchenna David <daviduchenna@outlook.com>
2017-09-15 01:16:06 +0100

    finished file upload and image creation

--- app/utils.py
+++ app/utils.py
@@ -1,7 +1,7 @@
 from PIL import Image, ImageDraw, ImageFont, ImageOps
 
 
-def generate_avi(**kwargs):
+def generate_avi(kwargs):
     W, H = (400,400)
 
 
@@ -10,15 +10,17 @@ def generate_avi(**kwargs):
     back.save('back.png')
 
     #pyconNG logo
-    logo = Image.open('logo.png')
+    logo = Image.open('app/static/logo.png')
+    # import pdb ; pdb.set_trace()
     w,h = logo.size
-    logo = logo.resize((w/2, h/2),Image.ANTIALIAS);
+    logo = logo.resize((int(w/2), int(h/2)),Image.ANTIALIAS);
     back.paste(logo, (15,15),logo)
     # back.save('back.png')
 
     bg_w, bg_h = back.size
     #downloaded user image
-    im = Image.open('image.jpeg')
+    # import pdb ; pdb.set_trace()
+    im = Image.open(kwargs['image'].name)
     im = im.resize((200,200),Image.ANTIALIAS)
     bigsize = (im.size[0] * 3, im.size[1] * 3)
     mask = Image.new('L', bigsize, 0)
@@ -29,7 +31,7 @@ def generate_avi(**kwargs):
     # im.save("a.png")
     img_w, img_h = im.size
 
-    offset = ((bg_w - img_w) / 2, (bg_h - img_h) / 2)
+    offset = (int((bg_w - img_w) / 2), int((bg_h - img_h) / 2))
 
     back.paste(im,offset,im)
 
@@ -38,15 +40,15 @@ def generate_avi(**kwargs):
 
     name = kwargs['title']
     draw = ImageDraw.Draw(back)
-    font = ImageFont.truetype("FiraCode-Bold.ttf",22)
-
+    font = ImageFont.truetype("app/static/FiraCode-Bold.ttf",22)
+    # import pdb ; pdb.set_trace()
     w, h = font.getsize(name)
 
 
 
     draw.text(((W-w)/2,(350)), name, fill=kwargs['fg_color'],font=font)
 
-    back.save("hello.png", "PNG")
+    back.save("avatar.png", "PNG")
 
     return True
 
--- app/views.py
+++ app/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from . import utils
+from .forms import AVIForm
+from django.http import HttpResponse
+import os
+from django.utils.encoding import smart_str
 
 # Create your views here.
 
@@ -7,7 +12,16 @@ from django.views.generic import TemplateView
 class PyAvi(TemplateView):
     template_name = 'index.html'
 
-    def post(self,request,*args,**kwargs):
+    def post(self, request, *args, **kwargs):
         
-        return ""
-        
+        form = AVIForm(request.POST, request.FILES)
+        if form.is_valid():
+            res = form.create_avi()     
+
+            with open("avatar.png", 'rb') as fh:
+                response = HttpResponse(fh.read(), content_type="image/png")
+                response['Content-Disposition'] = 'attachmentq; filename=' + os.path.basename("avatar.png")
+                response['X-Sendfile'] = smart_str("avatar.png")
+                return response
+
+        return render(request,'index.html',{"error":True})
